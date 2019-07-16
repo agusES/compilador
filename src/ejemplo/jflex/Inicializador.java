f66abaf964b8b8796700f9501bf54e6f59dc2ed3
agusES <agussosa@gmail.com>
2019-07-16 17:41:30 -0300

    tabla de simbolos en proceso

---
+++ src/ejemplo/jflex/Inicializador.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo.jflex;
+
+/**
+ *
+ * @author Flynn
+ */
+public class Inicializador<T> {
+    private static final int valorEntero = 0;
+    private static float valorFloat = 0.0f;
+    private static boolean valorBooleano = false;
+    private T valor;
+    private T tipoParametro;
+    
+    public Inicializador(Class<T> tipo) {
+        switch(tipo.class) {
+            case "int":
+                this
+        }
+                
+    }
+    
+}
